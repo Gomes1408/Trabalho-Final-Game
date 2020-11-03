3da8129901f8379f17651fd86d06b2b799ccf23e
Tiago Torres Schmidt <ttschmidt@inf.ufrgs.br>
2020-11-03 18:58:15 -0300

    Ampliação da base do allegro, adição de objetos e propriedades.

--- game.c
+++ game.c
@@ -23,23 +23,86 @@ int main()
     bool done = false;
     bool redraw = true;
     ALLEGRO_EVENT event;
-    float x, y;
-    x = 0;
-    y = 0;    
+    float x, y;      
+    typedef struct {
+        int body;
+        int shiftable;
+        int state;
+        int colectable;
+        int player;
+        char identity;
+    }obj;
     unsigned char tecla[ALLEGRO_KEY_MAX];
     char map[11][27] = {{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
-                        {'J','M','M','M','#','M','M','M','M','#','M','M','M','M','#','M','#','#','#','#','#','#','#','M','M','C','#'},
+                        {'#','J','M','#','#','M','M','B','M','#','M','M','M','M','#','M','#','#','#','#','#','#','#','M','M','C','#'},
+                        {'#','#','A','M','M','M','#','#','M','#','M','#','#','#','#','M','#','#','#','#','#','#','#','#','#','#','#'},
+                        {'#','#','C','#','#','M','#','#','M','#','M','M','M','#','#','M','#','#','#','#','#','#','#','#','#','#','#'},
                         {'#','#','M','#','#','M','#','#','M','#','M','#','#','#','#','M','#','#','#','#','#','#','#','#','#','#','#'},
-                        {'#','#','M','#','#','M','#','#','M','#','M','M','M','#','#','M','#','#','#','#','#','#','#','#','#','#','#'},
-                        {'#','#','M','#','#','M','#','#','M','#','M','#','#','#','#','M','#','#','#','#','#','#','#','#','#','#','#'},
-                        {'#','M','M','#','#','M','M','M','M','#','M','M','M','M','#','M','M','M','M','#','#','#','#','#','#','#','#'},
+                        {'#','M','D','#','#','M','M','M','M','#','M','M','M','M','#','M','M','M','M','#','#','#','#','#','#','#','#'},
                         {'#','#','#','M','M','M','#','M','M','M','#','#','M','#','#','M','#','M','M','M','M','#','M','#','#','#','#'},
                         {'#','#','#','M','#','#','#','M','#','M','#','#','M','#','#','M','#','M','#','#','#','#','M','#','#','#','#'},
                         {'#','#','#','M','#','#','#','M','M','#','#','#','M','#','#','M','#','M','M','M','#','#','M','#','#','#','#'},
                         {'#','#','#','M','#','#','#','M','#','M','#','#','M','#','#','M','#','M','#','#','#','#','M','#','#','#','#'},
                         {'#','#','#','M','M','M','#','M','#','#','M','#','M','M','M','M','#','M','M','M','M','#','M','M','M','M','#'}}; 
-    char mapImage[11][27];
-    memcpy(mapImage,map, 11*27*sizeof(char));
+    obj objImage[11][27];
+
+    for(i=0;i<27;i++){
+        for(j=0;j<11;j++){
+            switch(map[j][i]){
+                case '#':
+                    objImage[j][i].body = 1;
+                    objImage[j][i].colectable = 0;
+                    objImage[j][i].player = 0;
+                    objImage[j][i].shiftable = 0;
+                    objImage[j][i].identity = '#';
+                    break;
+                case 'C':
+                    objImage[j][i].body = 0;
+                    objImage[j][i].colectable = 1;
+                    objImage[j][i].player = 0;
+                    objImage[j][i].shiftable = 0;
+                    objImage[j][i].identity = 'C';
+                    break;
+                case 'A':
+                    objImage[j][i].body = 0;
+                    objImage[j][i].colectable = 0;
+                    objImage[j][i].player = 0;
+                    objImage[j][i].shiftable = 1;
+                    objImage[j][i].identity = 'A';
+                    break;
+                case 'D':
+                    objImage[j][i].body = 1;
+                    objImage[j][i].colectable = 0;
+                    objImage[j][i].player = 0;
+                    objImage[j][i].shiftable = 1;
+                    objImage[j][i].identity = 'D';
+                    break;
+                case 'J':
+                    objImage[j][i].body = 0;
+                    objImage[j][i].colectable = 0;
+                    objImage[j][i].player = 1;
+                    objImage[j][i].shiftable = 0;
+                    objImage[j][i].identity = 'J';
+                    y = j*50;
+                    x = i*50;
+                    break;
+                case 'M':
+                    objImage[j][i].body = 0;
+                    objImage[j][i].colectable = 1;
+                    objImage[j][i].player = 0;
+                    objImage[j][i].shiftable = 0;
+                    objImage[j][i].identity = 'M';
+                    break;
+                case 'B':
+                    objImage[j][i].body = 0;
+                    objImage[j][i].colectable = 0;
+                    objImage[j][i].player = 0;
+                    objImage[j][i].shiftable = 1;
+                    objImage[j][i].identity = 'B';
+                    break;
+            }
+        }                
+    }
 
     inicializa(al_init(), "Allegro.");
     inicializa(al_install_keyboard(), "Teclado.-+");
@@ -69,8 +132,12 @@ int main()
     {
         al_wait_for_event(queue, &event);
                 
-        if(mapImage[(int)y/50][(int)x/50] == 'M'){
-            mapImage[(int)y/50][(int)x/50] = '#';
+        if((objImage[(int)y/50][(int)x/50].identity == 'M') || (objImage[(int)y/50][(int)x/50].identity == 'C')){
+            objImage[(int)y/50][(int)x/50].identity = ' ';
+            objImage[(int)y/50][(int)x/50].body = 0;
+            objImage[(int)y/50][(int)x/50].colectable = 0;
+            objImage[(int)y/50][(int)x/50].player = 0;
+            objImage[(int)y/50][(int)x/50].shiftable = 0;
             scoreCounter++;
         }
 
@@ -78,25 +145,25 @@ int main()
         {
             case ALLEGRO_EVENT_TIMER:
                 if(tecla[ALLEGRO_KEY_UP]){
-                    if(y == 0){}else{
+                    if(y == 0 || (objImage[(int)(y-50)/50][(int)x/50].body == 1)){}else{
                         y-=50;
                     }
                 }
                    
                 if(tecla[ALLEGRO_KEY_DOWN]){
-                    if(y == 10*50){}else{
+                    if(y == 10*50 || (objImage[(int)(y+50)/50][(int)x/50].body == 1)){}else{
                         y+=50;
                     }
                 }
             
                 if(tecla[ALLEGRO_KEY_LEFT]){
-                    if(x == 0){}else{
+                    if(x == 0 || (objImage[(int)y/50][(int)(x-50)/50].body == 1)){}else{
                         x-=50;
                     }
                 }
                 
                 if(tecla[ALLEGRO_KEY_RIGHT]){
-                    if(x == 26*50){}else{
+                    if(x == 26*50 || (objImage[(int)y/50][(int)(x+50)/50].body == 1)){}else{
                         x+=50;
                     }
                 }
@@ -104,8 +171,24 @@ int main()
                 if(tecla[ALLEGRO_KEY_ESCAPE]){
                     done = true;
                 }
-           
 
+                if(tecla[ALLEGRO_KEY_B] && ((objImage[(int)y/50][(int)x/50].identity == 'B') || (objImage[(int)(y-50)/50][(int)x/50].identity == 'B') || (objImage[(int)(y+50)/50][(int)x/50].identity == 'B') || (objImage[(int)y/50][(int)(x-50)/50].identity == 'B')|| (objImage[(int)y/50][(int)(x+50)/50].identity == 'B'))){
+                    for(i=0;i<27;i++){
+                        for(j=0;j<11;j++){
+                            if(objImage[j][i].identity == 'A'){
+                                objImage[j][i].identity = 'D';
+                                objImage[j][i].body = 1;
+                                }
+                            else if(objImage[j][i].identity == 'D'){
+                                objImage[j][i].identity = 'A';
+                                objImage[j][i].body = 0;
+                            }
+                        }                
+                    }
+                }
+           
+char mapImage[11][27];
+    memcpy(mapImage,map, 11*27*sizeof(char));
                 for(int i = 0; i < ALLEGRO_KEY_MAX; i++)
                     tecla[i] &= KEY_SEEN;
 
@@ -132,18 +215,30 @@ int main()
             al_clear_to_color(al_map_rgb(0, 0, 0));
             for(i=0;i<27;i++){
                 for(j=0;j<11;j++){
-                    switch(mapImage[j][i]){
+                    switch(objImage[j][i].identity){
                         case '#':
                             al_draw_filled_rectangle(i*50, j*50, i*50 + 49 , j*50 + 49 , al_map_rgb(172, 172, 172));
                             break;
                         case 'J':
                              al_draw_filled_rectangle(i*50, j*50, i*50 + 49 , j*50 + 49 , al_map_rgb(137, 137, 137));
                             break;
+                        case 'C':
+                             al_draw_filled_rectangle(i*50, j*50, i*50 + 49 , j*50 + 49 , al_map_rgb(217,217,25));
+                            break;
                         case 'M':
                             al_draw_filled_rectangle(i*50, j*50, i*50 + 49 , j*50 + 49 , al_map_rgb(255, 247, 0));
                             break;
                         case 'B':
                             al_draw_filled_rectangle(i*50, j*50, i*50 + 49 , j*50 + 49 , al_map_rgb(202, 135, 0));
+                            break;
+                        case 'A':
+                            al_draw_filled_rectangle(i*50, j*50, i*50 + 49 , j*50 + 49 , al_map_rgb(56 , 176, 222));
+                            break;
+                        case 'D':
+                            al_draw_filled_rectangle(i*50, j*50, i*50 + 49 , j*50 + 49 , al_map_rgb(255 , 0, 0));
+                            break;
+                        case ' ':
+                            al_draw_filled_rectangle(i*50, j*50, i*50 + 49 , j*50 + 49 , al_map_rgb(0, 0, 0));
                             break;
                     }
                 }                
